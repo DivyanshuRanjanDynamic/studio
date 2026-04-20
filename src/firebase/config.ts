70180ea1eb35b4287aaf28d75bd55b4f33f66c6b
DivyanshuRanjanDynamic <divyanshuchannel2@gmail.com>
2026-04-20 23:26:41 +0530

    config changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 export const firebaseConfig = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  appId: process.env.APP_ID,
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  measurementId: '',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 export const db = getFirestore(app);
+
